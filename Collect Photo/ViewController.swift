719b0df8240543fbecd8deeb210f07a026660599
Hiroki Naito <naitouswift@gmail.com>
2019-11-28 13:04:27 +0900

    ２＊３のセル表示、TabBarを継承したViewController

--- Collect Photo/ViewController.swift
+++ Collect Photo/ViewController.swift
@@ -8,17 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITabBarDelegate{
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
     
-    @IBOutlet weak var TabBar: UITabBar!
+    private let sectionInsets = UIEdgeInsets(top: 10.0, left: 2.0, bottom: 2.0, right: 2.0)
     
-
+    //１行あたり
+    private let itemsPerRow: CGFloat = 2
+  
+    @IBOutlet weak var collectionview: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        TabBar.delegate = self
     }
+    //要素数
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+          return 10
+      }
+      
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+          let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) //
+              cell.backgroundColor = .red  // セルの色
+          return cell
+      }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+            let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+            let availableWidth = view.frame.width - paddingSpace
+            let widthPerItem = availableWidth / itemsPerRow
+            return CGSize(width: widthPerItem , height: widthPerItem)
+       }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+           return sectionInsets
+       }
+       // セルの行間の設定
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+           return 10.0
+       }
+    
 
 
 }
