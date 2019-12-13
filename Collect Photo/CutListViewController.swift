a1d8cf625d84351e29350e36a7874c67c77bc3d5
Hiroki Naito <naitouswift@gmail.com>
2019-12-14 03:21:50 +0900

    設定画面の構築終了。

--- Collect Photo/CutListViewController.swift
+++ Collect Photo/CutListViewController.swift
@@ -25,15 +25,14 @@ class CutListViewController: UIViewController,UITableViewDelegate,UITableViewDat
         if let storedMemberList = userDefaults.array(forKey: "cutArray") as? [String] {
             cutArray.append(contentsOf: storedMemberList)
         }
-        // Do any additional setup after loading the view.
+        
     }
     
     
     @IBAction func cutAdd(_ sender: Any) {
         
         var textField = UITextField()
-        let alert = UIAlertController(title: "種類を追加", message: "", preferredStyle: .alert)
-        
+        let alert = UIAlertController(title: "種類を追加", message: "生写真の型を追加しよう！", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "種類に追加", style: .default) { (action) in
         let newItem: String = String(textField.text!)
@@ -46,17 +45,16 @@ class CutListViewController: UIViewController,UITableViewDelegate,UITableViewDat
         userDefaults.synchronize()
         }
 
-        
         alert.addTextField { (alertTextField) in
         alertTextField.placeholder = "例 ヨリ"
         textField = alertTextField
         }
 
-        
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
         
     }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return cutArray.count
