ac0129686cae00073645b71524447c3d1be5d958
Hiroki Naito <naitouswift@gmail.com>
2019-12-18 13:52:32 +0900

    チェックマーク。複数選択。チェックマーク削除。完了。

--- Collect Photo/SelectMemberViewController.swift
+++ Collect Photo/SelectMemberViewController.swift
@@ -13,6 +13,8 @@ class SelectMemberViewController: UIViewController,UITableViewDelegate,UITableVi
     
     var memberArray:Results<MemberData>!
     
+    var rowListArray = [Int]()
+    
     //メンバー変数
     var members:String = ""
     var membersLabel = UILabel()
@@ -30,7 +32,7 @@ class SelectMemberViewController: UIViewController,UITableViewDelegate,UITableVi
         
         tableview.delegate = self
         tableview.dataSource = self
-
+        tableview.allowsMultipleSelectionDuringEditing = true
         // Do any additional setup after loading the view.
         
         do{
@@ -41,50 +43,84 @@ class SelectMemberViewController: UIViewController,UITableViewDelegate,UITableVi
         
         
     }
-    
+    /*
     override func viewWillAppear(_ animated: Bool) {
+        
         super.viewWillAppear(animated)
         tableview.reloadData()
-    }
+        
+    }*/
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-           return memberArray.count
-           
-       }
-       
-       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-           let cell = tableView.dequeueReusableCell(withIdentifier: "MemberCell", for: indexPath)
-                  let item = memberArray[indexPath.row].Member
-                  
-                  colorLabel = cell.contentView.viewWithTag(7) as! UILabel
-                  
-                  membersLabel = cell.contentView.viewWithTag(8) as! UILabel
-                  membersLabel.text = item
-                  
-                  switch memberArray[indexPath.row].Color {
-                      
-                  case 1:
-                      colorLabel.backgroundColor = .red
-                  case 2:
-                      colorLabel.backgroundColor = .purple
-                  case 3:
-                      colorLabel.backgroundColor = .systemTeal
-                  case 4:
-                      colorLabel.backgroundColor = .green
-                  case 5:
-                      colorLabel.backgroundColor = .yellow
-                  case 6:
-                      colorLabel.backgroundColor = .black
-                  default:
-                      colorLabel.backgroundColor = .white
-                      
-                  }
         
-            return cell
-       }
+        return memberArray.count
+        
+    }
+       
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemberCell", for: indexPath)
+        let item = memberArray[indexPath.row].Member
+                  
+        colorLabel = cell.contentView.viewWithTag(7) as! UILabel
+                  
+        membersLabel = cell.contentView.viewWithTag(8) as! UILabel
+        membersLabel.text = item
+    //セルの選択時の背景色を消す
+        cell.selectionStyle = UITableViewCell.SelectionStyle.none
+        
+        
+        switch memberArray[indexPath.row].Color {
+                      
+            case 1:
+                colorLabel.backgroundColor = .red
+            case 2:
+                colorLabel.backgroundColor = .purple
+            case 3:
+                colorLabel.backgroundColor = .systemTeal
+            case 4:
+                colorLabel.backgroundColor = .green
+            case 5:
+                colorLabel.backgroundColor = .yellow
+            case 6:
+                colorLabel.backgroundColor = .black
+            default:
+                colorLabel.backgroundColor = .white
+                      
+            }
+                //  セルの選択状況の判定
+        if (rowListArray.contains(indexPath.row)){
+            cell.accessoryType = .checkmark
+        }else{
+            cell.accessoryType = .none
+        }
+        
+        return cell
+        
+    }
     
     
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        
+        //  選択したセルにチェックマークが無い場合
+        if(cell?.accessoryType == UITableViewCell.AccessoryType.none){
+            cell?.accessoryType = .checkmark
+            self.rowListArray.append(indexPath.row)
+            
+        }else{
+            
+        //  選択したセルにチェックマークがある場合
+            cell?.accessoryType = .none
+            let  listNumber = rowListArray.filter ({ (n:Int) -> Bool in
+            if n != indexPath.row{
+                return true
+            }else{
+                return false
+            }})
+            rowListArray = listNumber
+        }
+        
+    }
     /*
     // MARK: - Navigation
 
@@ -94,5 +130,4 @@ class SelectMemberViewController: UIViewController,UITableViewDelegate,UITableVi
         // Pass the selected object to the new view controller.
     }
     */
-
 }
